a109791f7333d3647533815278cf64e290778ac6
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-10-05 07:15:11 +0100

    Successfully got the music from the device

--- app/src/main/java/bleezzermusic/bleezzermusicplayer/MainActivity.java
+++ app/src/main/java/bleezzermusic/bleezzermusicplayer/MainActivity.java
@@ -1,8 +1,11 @@
 package bleezzermusic.bleezzermusicplayer;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +18,10 @@ import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
-    //THE SONG WILL BE STORED IN AN ARRAYLIST
+    //THE SONG WILL BE STORED IN THIS ARRAYLIST
     ArrayList<songsQuery> songArrayList;
 
-    //CREATED A GLOBAL VARIABLE OF THE  LISTVIEW
+    //CREATED A GLOBAL VARIABLE OF THE  LIST VIEW
     private ListView listView;
 
     @Override
@@ -26,7 +29,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //GETTING THE INSTANCE OF THE LISTVIEW ID
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                }, 1);
+                return;
+            }
+        }
+        //GETTING THE INSTANCE OF THE LIST VIEW ID
         listView = findViewById(R.id.song_list);
 
         //INSTANTIATE THE  ARRAYLIST
--- app/src/main/java/bleezzermusic/bleezzermusicplayer/songAdapter.java
+++ app/src/main/java/bleezzermusic/bleezzermusicplayer/songAdapter.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,13 +37,23 @@ public class songAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LinearLayout linearLayout = (LinearLayout) layoutInflater.inflate(R.layout.music_layout, parent, false);
-        TextView songName = linearLayout.findViewById(R.id.song_name);
-        TextView songArtist = linearLayout.findViewById(R.id.song_artist);
+
+        //GET THE LAYOUT THAT WILL BE USED IN FORMING THE SONG LAYOUT
+        RelativeLayout relativeLayout = (RelativeLayout) layoutInflater.inflate(R.layout.music_layout, parent, false);
+
+        //THEN GET THE INSTANCE OF THE TWO VIEWS; THAT IS THE SONG NAME AND SONG ARTIST
+        TextView songName = relativeLayout.findViewById(R.id.song_name);
+        TextView songArtist = relativeLayout.findViewById(R.id.song_artist);
+
+        // GET THE SONG POSITION, SO THAT WE CAN KNOW WHICH OF THE  SONG WAS CLICKED
         songsQuery getCurrentSongs = songs.get(position);
-        songName.setText(getCurrentSongs.getTitle());
-        songArtist.setText(getCurrentSongs.getArtist());
-        linearLayout.setTag(position);
-        return linearLayout;
+
+        //SET THE SONGS INTO THE TEXT VIEW, USING SELECTED MATERIAL
+        songName.setText(getCurrentSongs.getArtist());
+        songArtist.setText(getCurrentSongs.getTitle());
+
+        relativeLayout.setTag(position);
+
+        return relativeLayout;
     }
 }
