68a56fb4d5d57d4d951fe221a07409a3733c68d3
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-10-07 15:17:14 +0100

    Music finally playing...

--- app/src/main/java/bleezzermusic/bleezzermusicplayer/MainActivity.java
+++ app/src/main/java/bleezzermusic/bleezzermusicplayer/MainActivity.java
@@ -1,14 +1,20 @@
 package bleezzermusic.bleezzermusicplayer;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -20,9 +26,30 @@ public class MainActivity extends AppCompatActivity {
 
     //THE SONG WILL BE STORED IN THIS ARRAYLIST
     ArrayList<songsQuery> songArrayList;
-
+    MusicService musicService;
     //CREATED A GLOBAL VARIABLE OF THE  LIST VIEW
     private ListView listView;
+    private Intent playIntent;
+    private boolean musicBound = false;
+    private ServiceConnection musicConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder) service;
+            musicService = binder.getService();
+
+            //TODO if there is problem, check this list
+
+            musicService.setList(songArrayList);
+
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            //musicService.setList(songArrayList);
+            musicBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +81,16 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(songAdapter);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
     private void getSongsFromDevice() {
 
         //CREATED A CONTENT RESOLVER INSTANCE, TO RETRIEVE THE URI FOR EXTERNAL MUSIC FILES
@@ -79,5 +116,10 @@ public class MainActivity extends AppCompatActivity {
             }
             while (musicCursor.moveToNext());
         }
+    }
+
+    public void songSelected(View view) {
+        musicService.setSong(Integer.parseInt(view.getTag().toString()));
+        musicService.playSong();
     }
 }
--- app/src/main/java/bleezzermusic/bleezzermusicplayer/MusicService.java
+++ app/src/main/java/bleezzermusic/bleezzermusicplayer/MusicService.java
@@ -1,14 +1,97 @@
 package bleezzermusic.bleezzermusicplayer;
 
 import android.app.Service;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
-public class MusicService extends Service {
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+
+    private final IBinder musicBind = new MusicBinder();
+    private MediaPlayer mediaPlayer;
+    private ArrayList<songsQuery> songsQueries;
+    private int songPosition;
+
+    public void onCreate() {
+        super.onCreate();
+        songPosition = 0;
+        mediaPlayer = new MediaPlayer();
+
+        initializeMusicPlayer();
+    }
+
+    public void setList(ArrayList<songsQuery> thesongs) {
+        songsQueries = thesongs;
+    }
+
+    public void initializeMusicPlayer() {
+        mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+    }
+
+    public void playSong() {
+        mediaPlayer.reset();
+        songsQuery songsQuery = songsQueries.get(songPosition);
+        long currentSong = songsQuery.getId();
+        Uri songUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, currentSong);
+
+        try {
+            mediaPlayer.setDataSource(getApplicationContext(), songUri);
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error setting data source", Toast.LENGTH_SHORT).show();
+        }
+        mediaPlayer.prepareAsync();
+    }
+
+    public void setSong(int songIdex) {
+        songPosition = songIdex;
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
     }
 }
