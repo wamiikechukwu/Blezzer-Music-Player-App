76c074eb384b64d0f388848cda1b77270bb548cf
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-10-21 18:01:59 +0100

    Added Splash Screen to the app (V 1.1)

--- app/src/main/java/bleezzermusic/bleezzermusicplayer/SplashScreen.java
+++ app/src/main/java/bleezzermusic/bleezzermusicplayer/SplashScreen.java
@@ -2,12 +2,25 @@ package bleezzermusic.bleezzermusicplayer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //HIDE THE TITLE BAR IN THE PHONE
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //HIDE THE ACTION BAR FOR THE SPLASH SCREEN
+        getSupportActionBar().hide();
+
+        //HIDE THE STATUS BAR IN THE PHONE
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
+
+
     }
 }
