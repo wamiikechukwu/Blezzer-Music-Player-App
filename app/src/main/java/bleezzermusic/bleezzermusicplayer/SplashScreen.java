f3a39cf34a2dfc4d5d7c620601f44282f0c5c28c
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-10-21 18:36:55 +0100

    Added Splash Screen to the app (DONE)

--- app/src/main/java/bleezzermusic/bleezzermusicplayer/SplashScreen.java
+++ app/src/main/java/bleezzermusic/bleezzermusicplayer/SplashScreen.java
@@ -1,10 +1,14 @@
 package bleezzermusic.bleezzermusicplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
+
+    int secondsDelayed = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,17 @@ public class SplashScreen extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash_screen);
+
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
 
 
     }
